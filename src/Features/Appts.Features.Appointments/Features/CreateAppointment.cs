6a72f75d158fe6e99b851123bf7f962a198bed91
Kaloyan Drenski <drenski666@gmail.com>
2024-06-15 09:41:23 +0300

    Moving everything inside CreateAppointment class

--- src/Features/Appts.Features.Appointments/Features/CreateAppointment.cs
+++ src/Features/Appts.Features.Appointments/Features/CreateAppointment.cs
@@ -5,70 +5,73 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Appts.Features.Appointments.Features;
-[HttpPost("api/appointments")]
-public class CreateAppointmentEndpoint : Endpoint<CreateAppointmentModel,
-                                   Results<Ok<CreateAppointmentResponseModel>,
-                                           NotFound,
-                                           ProblemDetails>>
+
+public class CreateAppointment
 {
-    private readonly IMediator _mediator;
+    public record CreateAppointmentModel(string Title, DateTimeOffset Start, DateTimeOffset End);
+    public record CreateAppointmentResponseModel(string Title, DateTimeOffset Start, DateTimeOffset End);
+    public record CreateAppointmentCommand(CreateAppointmentModel Model) : IRequest<CreateAppointmentResponseModel>;
 
-    public CreateAppointmentEndpoint(IMediator mediator)
+    [HttpPost("api/appointments")]
+    public class CreateAppointmentEndpoint : Endpoint<CreateAppointmentModel,
+                                       Results<Ok<CreateAppointmentResponseModel>,
+                                               NotFound,
+                                               ProblemDetails>>
     {
-        _mediator = mediator;
-    }
+        private readonly IMediator _mediator;
 
-    public override async Task<Results<Ok<CreateAppointmentResponseModel>,
-                                           NotFound,
-                                           ProblemDetails>> ExecuteAsync(CreateAppointmentModel request, CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(new CreateAppointmentCommand(request));
-
-        if (result != null)
+        public CreateAppointmentEndpoint(IMediator mediator)
         {
-            return TypedResults.Ok(result);
+            _mediator = mediator;
         }
 
-        AddError("Unable to create an appointment");
+        public override async Task<Results<Ok<CreateAppointmentResponseModel>,
+                                               NotFound,
+                                               ProblemDetails>> ExecuteAsync(CreateAppointmentModel request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new CreateAppointmentCommand(request));
 
-        return new FastEndpoints.ProblemDetails(ValidationFailures);
+            if (result != null)
+            {
+                return TypedResults.Ok(result);
+            }
+
+            AddError("Unable to create an appointment");
+
+            return new FastEndpoints.ProblemDetails(ValidationFailures);
+        }
+
+
+    }
+
+    private class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, CreateAppointmentResponseModel>
+    {
+        private readonly AppointmentsDbContext _dbContext;
+
+        public CreateAppointmentCommandHandler(AppointmentsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<CreateAppointmentResponseModel> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
+        {
+            _dbContext.Appointments.Add(new Appointment
+            {
+                Title = request.Model.Title,
+                Start = request.Model.Start,
+                End = request.Model.End
+            });
+
+            var result = await _dbContext.SaveChangesAsync() > 0;
+
+            if (result)
+            {
+                return new CreateAppointmentResponseModel(request.Model.Title, request.Model.Start, request.Model.End);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
-
-public record CreateAppointmentModel(string Title, DateTimeOffset Start, DateTimeOffset End);
-
-public record CreateAppointmentResponseModel(string Title, DateTimeOffset Start, DateTimeOffset End);
-
-public record CreateAppointmentCommand(CreateAppointmentModel Model) : IRequest<CreateAppointmentResponseModel>;
-
-internal class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, CreateAppointmentResponseModel>
-{
-    private readonly AppointmentsDbContext _dbContext;
-
-    public CreateAppointmentCommandHandler(AppointmentsDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
-    public async Task<CreateAppointmentResponseModel> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
-    {
-        _dbContext.Appointments.Add(new Appointment
-        {
-            Title = request.Model.Title,
-            Start = request.Model.Start,
-            End = request.Model.End
-        });
-
-        var result = await _dbContext.SaveChangesAsync() > 0;
-
-        if (result)
-        {
-            return new CreateAppointmentResponseModel(request.Model.Title, request.Model.Start, request.Model.End);
-        }
-        else
-        {
-            return null;
-        }
-    }
-}
-
