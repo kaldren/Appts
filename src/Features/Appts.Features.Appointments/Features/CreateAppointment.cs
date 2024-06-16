cdf5fef651f3f4573d28df41860f51106ec0575b
Kaloyan Drenski <drenski666@gmail.com>
2024-06-16 13:16:59 +0300

    wip

--- src/Features/Appts.Features.Appointments/Features/CreateAppointment.cs
+++ src/Features/Appts.Features.Appointments/Features/CreateAppointment.cs
@@ -1,9 +1,12 @@
 ﻿using Appts.Features.Appointments.Models;
+using Appts.Shared;
 using FastEndpoints;
 using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace Appts.Features.Appointments.Features;
 
@@ -36,24 +39,28 @@ public class CreateAppointment
 
                 return new ProblemDetails(ValidationFailures);
             }
+
             var result = await _mediator.Send(new CreateAppointmentCommand(request));
 
-            if (result != null)
+            if (result.IsSuccess)
             {
-                return TypedResults.Ok(result);
+                return TypedResults.Ok(result.Payload);
             }
-
-            AddError("Unable to create an appointment");
-
-            return new ProblemDetails(ValidationFailures);
+            else
+            {
+                return new ProblemDetails(new List<ValidationFailure>
+                {
+                    new("validation error", result.FailureReason)
+                });
+            }
         }
     }
 
     public record CreateAppointmentRequestModel(string Title, DateTimeOffset Start, DateTimeOffset End);
     public record CreateAppointmentResponseModel(string Title, DateTimeOffset Start, DateTimeOffset End);
-    public record CreateAppointmentCommand(CreateAppointmentRequestModel Model) : IRequest<CreateAppointmentResponseModel>;
+    public record CreateAppointmentCommand(CreateAppointmentRequestModel Model) : IRequest<CommandResult<CreateAppointmentResponseModel>>;
 
-    private class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, CreateAppointmentResponseModel>
+    private class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, CommandResult<CreateAppointmentResponseModel>>
     {
         private readonly AppointmentsDbContext _dbContext;
 
@@ -62,8 +69,20 @@ public class CreateAppointment
             _dbContext = dbContext;
         }
 
-        public async Task<CreateAppointmentResponseModel> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
+        public async Task<CommandResult<CreateAppointmentResponseModel>> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
         {
+            // Check if the appointment already exists
+            var appointmentExists = await _dbContext.Appointments.AnyAsync(x => x.Title == request.Model.Title && x.Start == request.Model.Start && x.End == request.Model.End);
+
+            // If the appointment already exists, return command result with error message
+            if (appointmentExists)
+                return CommandResult<CreateAppointmentResponseModel>.Failure("Appointment already exists");
+
+            // If start date is greater than end date, return command result with error message
+            if (request.Model.Start > request.Model.End)
+                return CommandResult<CreateAppointmentResponseModel>.Failure("Start date cannot be greater than end date");
+
+            // Add the appointment to the database
             _dbContext.Appointments.Add(new Appointment
             {
                 Title = request.Model.Title,
@@ -73,14 +92,11 @@ public class CreateAppointment
 
             var result = await _dbContext.SaveChangesAsync() > 0;
 
-            if (result)
-            {
-                return new CreateAppointmentResponseModel(request.Model.Title, request.Model.Start, request.Model.End);
-            }
-            else
-            {
-                return null;
-            }
+            // If the appointment was not added to the database, return command result with error message
+            if (!result)
+                return CommandResult<CreateAppointmentResponseModel>.Failure("Unable to create an appointment");
+            else // If the appointment was added to the database, return command result with the appointment details
+                return CommandResult<CreateAppointmentResponseModel>.Success(new CreateAppointmentResponseModel(request.Model.Title, request.Model.Start, request.Model.End));
         }
     }
     private class CreateAppointmentValidator : AbstractValidator<CreateAppointmentRequestModel>
---
+++ src/Shared/Appts.Shared/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Appts.Shared;
+
+public class CommandResult<T>
+{
+    private CommandResult(string reason) => FailureReason = reason;
+    private CommandResult(T payload) => Payload = payload;
+
+    public T Payload { get; }
+    public string FailureReason { get; }
+    public bool IsSuccess => FailureReason == null;
+
+    public static CommandResult<T> Success(T payload) => new(payload);
+    public static CommandResult<T> Failure(string reason) => new(reason);
+    public static implicit operator bool(CommandResult<T> result) => result.IsSuccess;
+}
